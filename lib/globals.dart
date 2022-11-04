cdfc4dcf786203615ba3b53b7bdc10208fe2df13
HeroS3cilritorno <117485676+HeroS3cilritorno@users.noreply.github.com>
2022-11-04 23:19:55 +0100

    primo commit

---
+++ lib/globals.dart
@@ -0,0 +1,16 @@
+/* Questo file contiene variabili che devono essere condivise da più file .dart */
+
+library sms_camera.globals;
+
+import 'home.dart';
+
+// attiva / disattiva messaggi di debug
+bool kDebugMode = true;
+
+var fun = FunctionManager();
+
+// variabili che indicano lo stato dei permessi (true = permesso concesso, false = permesso negato)
+bool smsPermissions = true;
+bool recorderPermissions = true;
+bool storagePermissions = true;
+bool cameraPermissions = true;
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,20 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import audio_session
+import audioplayers
+import flutter_local_notifications
+import path_provider_macos
+import url_launcher_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  AudioplayersPlugin.register(with: registry.registrar(forPlugin: "AudioplayersPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+}
